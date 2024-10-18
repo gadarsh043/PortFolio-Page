c9561e171476555f9bc97ac619db9962e9f8dd83
Adarsh Sonu <adarshsonu@Adarshs-MacBook-Pro.local>
2024-10-17 19:58:06 -0500

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <AppRoutes />
     </Router>
   );

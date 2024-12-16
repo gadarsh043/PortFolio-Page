f84f5dc029303677c447397d1c7a09dec3bdba08
Adarsh Sonu <adarshsonu@Mac.lan>
2024-12-16 02:35:09 -0600

    changes in scss

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "@radix-ui/themes/styles.css";
+import './main.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
